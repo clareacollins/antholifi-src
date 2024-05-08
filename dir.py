e2f5e60f704ea5bcaaae1a1e0fb47370d4ffa19f
clareacollins <119975593+clareacollins@users.noreply.github.com>
2024-05-08 16:28:24 -0400

    Create dir.py

---
+++ dir.py
@@ -0,0 +1,38 @@
+root = 'REDACTED'
+
+# Active Position
+pos = root + '\\os'
+ext = pos + '\\ext'
+pos_temp = pos + '\\temp'
+
+# Libraries
+lib_root = root + '\\lib'
+anth = lib_root + '\\anth'
+book = lib_root + '\\book'
+misc = lib_root + '\\misc'
+
+# Epubs
+epub_root = root + '\\epub'
+anth_epub = epub_root + '\\anth'
+anth_cont = epub_root + '\\cont'
+lost = epub_root + '\\lost'
+dest = epub_root + '\\dest'
+epub_temp = epub_root + '\\temp'
+
+# Images
+img_root = root + '\\img'
+icon = img_root + '\\icon'
+cover = img_root + '\\cover'
+anth_img = img_root + '\\anth'
+misc_img = img_root + '\\misc'
+temp_img = img_root + '\\temp'
+
+# Icons
+left_arrow_icon = icon + '\\left_arrow.png'
+right_arrow_icon = icon + '\\right_arrow.png'
+up_arrow_icon = icon + '\\up_arrow.png'
+down_arrow_icon = icon + '\\down_arrow.png'
+
+# Scraper
+downloads = 'REDACTED'
+storage = 'REDACTED'
